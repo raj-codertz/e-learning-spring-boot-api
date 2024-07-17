457a28629dee3b0de5b145f75a980b9889dfd7c7
Rajabu Shabani <rajabujshabani@gmail.com>
2024-07-17 21:13:05 +0300

    create repositories

--- src/main/java/com/suinegraj/datajpa/models/Author.java
+++ src/main/java/com/suinegraj/datajpa/models/Author.java
@@ -1,10 +1,9 @@
 package com.suinegraj.datajpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 
 @Data
@@ -16,51 +15,32 @@ public class Author {
     @GeneratedValue
     private Integer id;
 
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
 
     private String lastName;
 
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
 
     private int age;
 
-    public Integer getId() {
-        return id;
-    }
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
