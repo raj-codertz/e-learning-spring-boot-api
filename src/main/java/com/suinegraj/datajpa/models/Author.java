7dfe983be4b3c5d33df7320bf865633cdbceb59a
Rajabu Shabani <rajabujshabani@gmail.com>
2024-07-17 17:32:40 +0300

    fix issue of db connection

--- src/main/java/com/suinegraj/datajpa/models/Author.java
+++ src/main/java/com/suinegraj/datajpa/models/Author.java
@@ -1,11 +1,27 @@
 package com.suinegraj.datajpa.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue
     private Integer id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
+
     private int age;
 
     public Integer getId() {
