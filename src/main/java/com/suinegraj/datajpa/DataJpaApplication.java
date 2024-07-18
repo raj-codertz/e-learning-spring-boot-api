ae8ce8ef670be77902cc7062a459a0d2a84269dd
Rajabu Shabani <rajabujshabani@gmail.com>
2024-07-18 15:57:51 +0300

    done with entities relationship

--- src/main/java/com/suinegraj/datajpa/DataJpaApplication.java
+++ src/main/java/com/suinegraj/datajpa/DataJpaApplication.java
@@ -1,13 +1,32 @@
 package com.suinegraj.datajpa;
 
+import com.suinegraj.datajpa.models.Author;
+import com.suinegraj.datajpa.repositories.AuthorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthorRepository repository
+    ) {
+        return args -> {
+            var author = Author.builder()
+                    .firstName("rashid")
+                    .email("rajabushabani@gmail.com")
+                    .lastName("ally")
+                    .age(29)
+                    .build();
+            repository.save(author);
+        };
     }
 
 }
--- src/main/java/com/suinegraj/datajpa/models/Author.java
+++ src/main/java/com/suinegraj/datajpa/models/Author.java
@@ -1,13 +1,18 @@
 package com.suinegraj.datajpa.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Author {
 
@@ -31,16 +36,8 @@ public class Author {
 
     private int age;
 
-    @Column(
-            updatable = false,
-            nullable = false
-    )
-    private LocalDateTime createdAt;
-
-    @Column(
-            insertable = false
-    )
-    private LocalDateTime lastModified;
+    @ManyToMany(mappedBy = "authors")
+    private List<Course> courses;
 
 
 }
