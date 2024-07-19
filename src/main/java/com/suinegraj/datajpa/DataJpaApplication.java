c849032309c72073b7ad175b371eb4bcc6c89806
Rajabu Shabani <rajabujshabani@gmail.com>
2024-07-19 14:35:56 +0300

    inheritance with single table strategy

--- src/main/java/com/suinegraj/datajpa/DataJpaApplication.java
+++ src/main/java/com/suinegraj/datajpa/DataJpaApplication.java
@@ -1,28 +1,31 @@
 package com.suinegraj.datajpa;
 
+import com.suinegraj.datajpa.models.Video;
+import com.suinegraj.datajpa.repositories.VideoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataJpaApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(
-//            AuthorRepository repository
-//    ) {
-//        return args -> {
-//            var author = Author.builder()
-//                    .firstName("rashid")
-//                    .email("rajabushabani@gmail.com")
-//                    .lastName("ally")
-//                    .age(29)
-//                    .build();
-//            repository.save(author);
-//        };
-//    }
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            VideoRepository videoRepository
+    ) {
+        return _ -> {
+             var video = Video.builder()
+                     .name("abc")
+                     .length(5)
+                     .build();
+                  videoRepository.save(video);
+        };
+    }
 
 }
---
+++ src/main/java/com/suinegraj/datajpa/repositories/VideoRepository.java
@@ -0,0 +1,8 @@
+package com.suinegraj.datajpa.repositories;
+
+import com.suinegraj.datajpa.models.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VideoRepository extends JpaRepository<Video, Integer> {
+
+}
